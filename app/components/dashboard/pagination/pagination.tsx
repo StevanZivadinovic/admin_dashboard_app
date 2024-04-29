9a4fb4f4e008441332dbb222116c708684615553
StevanZivadinovic <stevanzivadinovic@outlook.com>
2024-04-29 15:34:59 +0200

    add new user func

--- app/components/dashboard/pagination/pagination.tsx
+++ app/components/dashboard/pagination/pagination.tsx
@@ -11,7 +11,7 @@ const Pagination = ({count}:PaginationType) => {
   const page = searchParams.get("page") || 1;
 
   const params = new URLSearchParams(searchParams);
-  const ITEM_PER_PAGE = 2;
+  const ITEM_PER_PAGE = 3;
 
   const hasPrev = ITEM_PER_PAGE * (parseInt(String(page)) - 1) > 0;
   const hasNext = ITEM_PER_PAGE * (parseInt(String(page)) - 1) + ITEM_PER_PAGE < count;
@@ -24,19 +24,19 @@ const Pagination = ({count}:PaginationType) => {
       : params.set("page", parseInt(String(page)) + 1);
     replace(`${pathname}?${params}`);
   };
-  const previousBtnClass = !hasPrev ? 'opacity-50 cursor-not-allowed':'opacity-100';
-  const nextBtnClass = !hasNext ? 'opacity-50 cursor-not-allowed':'opacity-100';
+  const previousBtnClass = !hasPrev ? 'opacity-50 cursor-not-allowed':'opacity-100 cursor-pointer';
+  const nextBtnClass = !hasNext ? 'opacity-50 cursor-not-allowed':'opacity-100 cursor-pointer';
   return (
     <div className='flex justify-between'>
       <button
-        className={`${previousBtnClass} bg-textSoft py-1 px-3 rounded-md cursor-pointer`}
+        className={`${previousBtnClass} bg-textSoft py-1 px-3 rounded-md `}
         disabled={!hasPrev}
         onClick={() => handleChangePage("prev")}
       >
         Previous
       </button>
       <button
-        className={`${nextBtnClass} bg-textSoft py-1 px-3 rounded-md cursor-pointer`}
+        className={`${nextBtnClass} bg-textSoft py-1 px-3 rounded-md`}
         disabled={!hasNext}
         onClick={() => handleChangePage("next")}
       >
